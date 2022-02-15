9df0549cd6ed6cf6a037c6db9201af28fb1baef0
Benjamin Lapointe-Pinel <benjamin.lapointe13@gmail.com>
2022-02-15 18:21:53 -0500

    first commit

---
+++ TP1_app_BLP/Model/Gender.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP1_app_BLP
+{
+    public enum Gender
+    {
+        Man,
+        Woman
+    }
+}
---
+++ TP1_app_BLP/View/Login.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TP1_app_BLP
+{
+    public partial class Login : Window
+    {
+        public List<Doctor> Doctors { get; private set; } = new List<Doctor> {
+            new("Benjamin", "Lapointe", new(1995, 11, 13), Gender.Man, new DateOnly(), "lapb0010@uqar.ca")
+        };
+
+        public Doctor? SelectedDoctor { get; set; }
+
+        public Login()
+        {
+            DataContext = this;
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
